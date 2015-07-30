b237442cc7a05be6a269913780d925b97058b77c
unknown <ujjwalvaish@outlook.com>
2015-07-30 14:57:33 +0530

    first commit

---
+++ SaveAqueryResultToDataTable/SaveAqueryResultToDataTable/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Data.SqlClient;
+using System.Data;
+namespace SaveAqueryResultToDataTable
+{
+    class Program
+    {
+        static void Main()
+        {
+            DataTable dataTable = new DataTable() ;
+            string connectionString = @"user id=US\uvaish;" +
+                                       @"server=USBLRUVAISH1\SQLEXPRESS;" +
+                                       "Trusted_Connection=yes;" +
+                                       "database=master; " +
+                                       "connection timeout=30";
+                SqlConnection connection = new SqlConnection(connectionString);
+                string stringSQLCommand = "Select * from student";
+                SqlCommand command = new SqlCommand(stringSQLCommand, connection);
+                connection.Open();
+                SqlDataAdapter adaptor = new SqlDataAdapter(command);
+                adaptor.Fill(dataTable);    
+                connection.Close();
+                adaptor.Dispose();
+                foreach (DataColumn column in dataTable.Columns)
+                {
+                    Console.Write(column.ColumnName + "\t");
+                }
+                foreach (DataRow row in dataTable.Rows)
+                {
+                    Console.WriteLine();
+                    for (int i = 0; i < dataTable.Columns.Count ; i++ )
+                    {
+                        Console.Write(row[i].ToString() + "\t");
+                    }
+                }                
+                Console.ReadKey();
+        }
+    }
+}
