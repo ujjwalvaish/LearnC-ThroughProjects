193b0ed878345ce5b2a0f3656ea43b79214e8499
unknown <ujjwalvaish@outlook.com>
2015-07-30 15:00:31 +0530

    first commit

---
+++ PassByReference/PassByReference/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+//Notice that "i" changes its value
+//This is known as PassByReference
+//This is because UserSelect does not make a copy of "i" and points to the same location as i
+//Also note that the static method MyMethod1 is called directly , without giving the class name , as it resides in the same class
+namespace PassByReference
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 0;
+            MyMethod1(ref i);
+            Console.WriteLine(i);
+            Console.ReadKey();
+        }
+        static void MyMethod1(ref int UserSelect)
+        {
+            UserSelect = 100;
+        }
+    }
+
+}
---
+++ PassByValue/PassByValue/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+//Notice that "i" does not change its value
+//This is known as PassByValue
+//This is because UserSelect makes a copy of "i" and changes its value to 100 , but original "i" remains the same
+//Also note that the static method MyMethod1 is called directly , without giving the class name , as it resides in the same class
+namespace PassByValue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 0;
+            MyMethod1(i);
+            Console.WriteLine(i);
+            Console.ReadKey();
+        }
+        static void MyMethod1(int UserSelect)
+        {
+            UserSelect = 100;
+        }
+    }
+
+}
